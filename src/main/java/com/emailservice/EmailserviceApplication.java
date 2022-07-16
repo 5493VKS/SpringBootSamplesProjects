0b739dc4633597b16b35ce2f6fcf20d6ce9bd178
5493VKS <1993vivek54@gmail.com>
2022-07-16 20:00:44 +0530

    Add all my files

---
+++ src/main/java/com/emailservice/EmailserviceApplication.java
@@ -0,0 +1,22 @@
+package com.emailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/emailservice")
+public class EmailserviceApplication {
+
+	@GetMapping("/send")
+	public String emailProcess() {
+		return "Send Email service";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(EmailserviceApplication.class, args);
+	}
+
+}
